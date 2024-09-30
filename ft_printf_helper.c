70715472ce5d4d4ca3f8ef729932bbed6dfe568b
shimi-be <sami.himi06@gmail.com>
2024-09-30 18:32:58 +0200

    Add files via upload

--- ft_printf_helper.c
+++ ft_printf_helper.c
@@ -6,7 +6,7 @@
 /*   By: shimi-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 16:29:11 by shimi-be          #+#    #+#             */
-/*   Updated: 2024/09/30 17:19:38 by shimi-be         ###   ########.fr       */
+/*   Updated: 2024/09/30 18:32:20 by shimi-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,28 @@
 #include <stdarg.h>
 void    print_num(int num, char type)
 {
+    unsigned long   nb;
     if (type == 'u')
     {
-        
+        nb = (unsigned long)num;
+        ft_putnbr(nb);
     }
     else
     {
-
+        ft_putnbr(num);
     }
 
 }
 
 void    print_dir(char  c)
 {
+    char    *dir;
 
+    dir = &c;
+    ft_putstr(dir);
 }
 
 void    print_str(char *str)
 {
-
+    ft_putstr(str);
 }
