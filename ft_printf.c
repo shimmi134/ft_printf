e2c12b6d1e4ef433032e8bc8dc7eb30587fd6109
shimmi134 <sami.himi06@gmail.com>
2024-10-04 20:26:13 +0200

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: shimi-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 16:25:02 by shimi-be          #+#    #+#             */
-/*   Updated: 2024/09/30 17:08:09 by shimi-be         ###   ########.fr       */
+/*   Updated: 2024/10/04 17:32:03 by shimi-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,50 +18,31 @@ int	ft_printf(char const *str, ...)
 {
     va_list args;
     int i;
-    char    *aux_str;
-    int aux_int;
-    char    aux_char;
 
     i = 0;
     va_start(args, str);
     while (str[i])
     {
         if (str[i] == '%')
-        {
-            if (str[i+1] == 'd' || str[i+1] == 'i' || str[i+1] == 'u' || str[i+1] == 'x' || str[i+1] == 'X')
-            {
-                aux_int = va_arg(args, int);
-                print_num(aux_int, str[i+1]);
-                i++;
-            }
-            else if(str[i+1] == 's')
-            {
-                aux_str = va_arg (args, char *);
-                print_str(aux_str);
-                i++;
-            }
-            else if ((str[i+1]) == 'p')
-            {
-                aux_str = va_arg(args, char *);
-                print_dir(aux_str);
-                i++;
-            }
-            else if (str[i+1] == 'c')
-            {
-                aux_char = va_arg(args, int);
-                write(1, &aux_char, 1);
-                i++;
-            }
-            else if (str[i+1] == '%')
-            {
-                write(1, &"%", 1);
-                i++;
-            }
-        }
+            choose_type(str[++i], args);
         else
             write(1, &str[i], 1);
         i++;
     }
     va_end(args);
     return (0);
+}
+
+void    choose_type(char c, va_list args)
+{    
+    if (c == 'd' || c == 'i' || c == 'u' || c == 'x' || c == 'X')
+        print_num(va_arg(args, int), c);
+    else if(c == 's')
+        print_str(va_arg(args, char *));
+    else if (c == 'p')
+        print_dir(va_arg(args, char*));
+    else if (c == 'c')
+        putchar(va_arg(args, int));
+    else if (c == '%')
+        putchar('%');
 }
